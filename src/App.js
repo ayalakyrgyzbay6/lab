df6e9afca4a305d6c5dd6691dd8cf560c00fe6b0
ayalakyrgyzbay6 <102906980+ayalakyrgyzbay6@users.noreply.github.com>
2022-04-11 14:47:01 +0600

    first

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import Header from './Header';
+import Nav from './Nav';
+import Main from './Main';
+import Aside from './Aside';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+    <Header/>
+    <Nav/>
+    <Main/>
+    <Aside/>
+    
+    
+  
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Nav.js
@@ -0,0 +1,16 @@
+import './Nav.css';
+
+const Nav = () => {
+  return(
+    <div className='Nav'>
+    <a href="">Главная</a>
+    <a href="">Оставить заявку</a>
+    <a href="">Каталог репетиторов</a>
+    <a href="">Отзывы</a>
+    <a href="">Контакты</a>
+    </div>
+  )
+}
+
+export default Nav;
+
